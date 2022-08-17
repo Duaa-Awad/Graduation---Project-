b0d7a7457df2fc01402d048a52826501da62e50c
Duaa Awad <92330147+Duaa-Awad@users.noreply.github.com>
2022-08-17 22:26:11 +0300

    Add files via upload

---
+++ Door-Sensor.ino
@@ -0,0 +1,28 @@
+int ledgreen = 26; //Door Closed Status
+int ledred = 12; // Door Open Status
+int sensor = 5; // Door sensor connected to Pin 6 in Arduino
+
+void setup()
+{
+  pinMode(ledgreen, OUTPUT);
+  pinMode(ledred, OUTPUT);
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (digitalRead(sensor) == HIGH)
+  {
+    digitalWrite(ledgreen, LOW);
+    digitalWrite(ledred, HIGH);
+    //Serial.println("Your Door is Closed");
+  }
+  else 
+  {
+    digitalWrite(ledgreen, HIGH);
+    digitalWrite(ledred, LOW);
+    //Serial.println("Your Door is Open");
+  }
+  delay(100);
+}
